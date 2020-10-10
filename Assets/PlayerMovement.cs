a01e6ee692ea4dbc05ddbd97f9501a14355e34e4
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-10-09 22:25:00 -0500

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D rb;
+    private Vector2 MoveVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 MoveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        MoveVelocity = MoveInput.normalized * Speed;
+    }
+}
