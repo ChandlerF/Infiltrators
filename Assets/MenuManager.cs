9ddc0db898b65b8531ad00efc1f5c3f509cadae3
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-10-11 19:18:01 -0500

    Started the Menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
