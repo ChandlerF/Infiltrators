41274a380232eba558519b37ee0088da1670b896
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-10-21 17:12:41 -0500

    MARINES MOVE (towards closest enemy)

--- Assets/Scripts/Marine.cs
+++ Assets/Scripts/Marine.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Marine : MonoBehaviour
 {
-    
+
+    public float Speed;
+    public FindingNearestEnemy RadarScript;
+    void Update()
+    {
+        if(RadarScript.ClosestEnemy != null)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, RadarScript.ClosestEnemy.position, Speed * Time.deltaTime);
+        }
+    }
 }
