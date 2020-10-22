9cc43a1d7e1b364d9a59075352a1f48298e861a8
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2020-10-21 21:44:54 -0500

    added stop distance to marines

--- Assets/Scripts/Marine.cs
+++ Assets/Scripts/Marine.cs
@@ -7,11 +7,18 @@ public class Marine : MonoBehaviour
 
     public float Speed;
     public FindingNearestEnemy RadarScript;
+    public float StopDistance;
+    //public float FollowDistance;
+
     void Update()
     {
         if(RadarScript.ClosestEnemy != null)
         {
-            transform.position = Vector2.MoveTowards(transform.position, RadarScript.ClosestEnemy.position, Speed * Time.deltaTime);
+            float DistanceBetween = Vector3.Distance(transform.position, RadarScript.ClosestEnemy.position);
+            if (DistanceBetween > StopDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, RadarScript.ClosestEnemy.position, Speed * Time.deltaTime);
+            }
         }
     }
 }
